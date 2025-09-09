d2c2add8314e083b770e2ff0b32c92be3fc5b793
Luiz Henrique <55117003+luxrail@users.noreply.github.com>
2025-09-09 10:50:53 -0300

    add filters

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,7 @@ export const dynamic = "force-dynamic"; // ou: export const revalidate = 0;
 import PostCard from "@/components/Posts";
 import { PaginationPosts } from "@/components/Pagination";
 import { getAllPosts } from "@/lib/wordpress";
+import FiltersBlog from "@/components/Filters";
 
 export default async function BlogPage({
   searchParams,
@@ -19,15 +20,20 @@ export default async function BlogPage({
   const data = await getAllPosts(8, searchQuery, currentPage);
 
   return (
-    <main className="container mx-auto px-4 py-8">
+    <main className="container mx-auto px-32 py-8">
       <h1 className="text-2xl font-bold mb-4">
         {searchQuery ? `Resultados para: ${searchQuery}` : "Todos os posts"}
       </h1>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
-        {data.posts.map((post: any) => (
-          <PostCard key={post.id} post={post} />
-        ))}
+      <div className="grid grid-cols-5 gap-6 ">
+        <FiltersBlog />
+        <div className="col-span-4">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {data.posts.map((post: any) => (
+              <PostCard key={post.id} post={post} />
+            ))}
+          </div>
+        </div>
       </div>
 
       <PaginationPosts
