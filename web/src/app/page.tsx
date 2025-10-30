cf968531c01f2e05c56aeb504643ad070f4d3b01
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2025-10-30 18:14:28 -0300

    feat: move Header and Footer components to HomePage for improved layout

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,13 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import Home from "@/components/Home";
 
 export default async function HomePage() {
     return (
         <div className="min-h-screen bg-background">
+            <Header />
             <Home />
+            <Footer />
         </div>
     );
 }
